385e3df2b98d9fba724dd1ed0842b74973edabe1
KeeganBess <keegan.bess@gmail.com>
2021-03-18 21:02:41 -0400

    attempt 1

--- client/src/views/tabs/ContactUs/ContactUs.js
+++ client/src/views/tabs/ContactUs/ContactUs.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './ContactUs.css'
+import { Link } from 'react-router-dom';
+//import Button from '../../components/Button'
 
 function ContactUs() {
+//     const [click, setClick] = useState(false);
+//     const [button, setButton]= useState(true);
+    
+//     const showButton= () => {
+//        if(window.innerWidth <=960){
+//             setButton(false);
+//        }
+//        else{
+//             setButton(true);
+//        }
+//        //<Button buttonStyle='btn-primary'>Model Recruitment Form</Button>
+//    };
+
+//     window.addEventListener('resize', showButton);
     return (
         <div className="App">
             <header className="App-header">
-                <p>
+                <h1>
                     Contact Us
-                </p>
+                </h1>
             </header>
+            <body className= "App-info">
+                <h2>Interested in joining DayDream Magazine?</h2>
+            </body>
+            
         </div>
     );
 }
