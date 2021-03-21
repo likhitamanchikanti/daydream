ca5dd2476d78633ea153c9c7336d80db58844b3c
annaraymaker <annaraymaker@ufl.edu>
2021-03-21 15:57:32 -0400

    dont add this to main its WIP

--- client/src/views/tabs/AboutUs/AboutUs.js
+++ client/src/views/tabs/AboutUs/AboutUs.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 function AboutUs() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
+        <div className="App-AU">
+            <header className="App-header-AU">
+                <h1>
                     About Us
-                </p>
+                </h1>
             </header>
+            <body className= "App-info-AU">
+                <h2>Who we are:</h2>
+                <a>We are a collective of artists and Gainesville community members.</a>
+                <h2>What we do:</h2>
+                <a>We create and share artistic content of all kinds in a virtual magazine linked in the homepage.</a>
+                <h2>Why we do it:</h2>
+                <a>To give an outlet to underrepresented and marginalized communities and the art they create. We wish to foster an 
+                    inclusive and unique environment that allows for the diversification of the Gainesville artistic community and beyond.
+                </a>
+            </body>
         </div>
     );
 }
 
 export default AboutUs;
+
+/*
+Who you are
+What matters to you
+What you do
+How you do it
+*/ 
